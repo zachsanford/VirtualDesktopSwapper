2cc48cba571971f4531462f7b6299a2c250a36fa
Zach Sanford <sanfordzachary@gmail.com>
2021-06-27 13:11:13 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using WindowsInput;
+using WindowsInput.Native;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace VirtualDesktopSwapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clear();
+            ShowTitle();
+            Thread.Sleep(1500);
+            int VDCount = GetVirtualDesktopCount();
+            int TimeBetween = GetTimeBetweenSwaps() * 1000;
+            Clear();
+            ShowTitle();
+            RunVirtualDesktopSwapper(VDCount, TimeBetween);
+        }
+
+        private static void ShowTitle()
+        {
+            WriteLine();
+            WriteLine("_/      _/  _/  _/_/_/   _/_/_/_/  _/    _/      _/      _/");
+            WriteLine(" _/    _/  _/  _/    _/    _/     _/    _/     _/_/     _/");
+            WriteLine("  _/  _/  _/  _/_/_/      _/     _/    _/    _/  _/    _/");
+            WriteLine("   _/_/  _/  _/    _/    _/     _/    _/   _/_/_/_/   _/");
+            WriteLine("    _/  _/  _/    _/    _/       _/_/    _/      _/  _/_/_/_/");
+            WriteLine();
+            WriteLine("    _/_/      _/_/_/_/  _/_/    _/   _/  _/_/_/_/   _/_/_/    _/_/_/");
+            WriteLine("   _/   _/   _/       _/   _/  _/  _/      _/     _/     _/  _/    _/");
+            WriteLine("  _/    _/  _/_/_/      _/    _/_/        _/     _/     _/  _/_/_/");
+            WriteLine(" _/   _/   _/       _/   _/  _/  _/      _/     _/     _/  _/");
+            WriteLine("_/_/_/    _/_/_/_/   _/_/   _/    _/    _/       _/_/_/   _/");
+            WriteLine();
+            WriteLine("   _/_/   _/        _/      _/      _/_/_/    _/_/_/    _/_/_/_/  _/_/_/");
+            WriteLine(" _/   _/  _/       _/     _/_/     _/    _/  _/    _/  _/        _/    _/");
+            WriteLine("   _/     _/  _/  _/    _/  _/    _/_/_/    _/_/_/    _/_/_/    _/_/_/");
+            WriteLine("_/   _/   _/_/ _/_/   _/_/_/_/   _/        _/        _/        _/    _/");
+            WriteLine(" _/_/     _/    _/  _/      _/  _/        _/        _/_/_/_/  _/    _/");
+            WriteLine();
+        }
+
+        private static int GetVirtualDesktopCount()
+        {
+            int _vdCount = 0;
+            bool _correctInput = false;
+
+            while (!_correctInput)
+            {
+                Write("Enter the total number of Virtual Desktops there are >> ");
+                _correctInput = Int32.TryParse(ReadLine(), out _vdCount);
+            }
+
+            WriteLine();
+            return _vdCount;
+        }
+
+        private static int GetTimeBetweenSwaps()
+        {
+            int _time = 0;
+            bool _correctInput = false;
+
+            while (!_correctInput)
+            {
+                Write("Please enter the amount of time (in seconds) between Virtual Desktop swaps >> ");
+                _correctInput = Int32.TryParse(ReadLine(), out _time);
+            }
+
+            WriteLine();
+            return _time;
+        }
+
+        private static void RunVirtualDesktopSwapper(int _vdCount, int _time)
+        {
+            InputSimulator _sim = new InputSimulator();
+            Write("Press Ctrl + C to quit...");
+
+            while (true)
+            {
+                for (int i = 0; i < _vdCount; i++)
+                {
+                    _sim.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.LWIN }, VirtualKeyCode.LEFT);
+                }
+
+                for (int i = 0; i < _vdCount; i++)
+                {
+                    Thread.Sleep(_time);
+                    _sim.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.LWIN }, VirtualKeyCode.RIGHT);
+                }
+            }
+        }
+    }
+}
